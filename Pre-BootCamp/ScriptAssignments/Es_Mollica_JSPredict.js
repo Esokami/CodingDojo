a9087a557b8462ee260217bfd8bc8d371ba1c334
Esokami <mollica.es@gmail.com>
2022-01-03 20:10:41 -0800

    Adding shared folders

---
+++ Pre-BootCamp/ScriptAssignments/Es_Mollica_JSPredict.js
@@ -0,0 +1,8 @@
+// Predict 1: I was born in 1980
+
+// Predict 2: I was born in 1980
+
+// Predict 3: Summing Numbers!
+//            Num1 is: 10
+//            Num2 is: 20
+//            30
---
+++ Pre-BootCamp/ScriptAssignments/Es_Mollica_JS_Tinkering.js
@@ -0,0 +1,12 @@
+
+function displayIfChildIsAbleToRideTheRollerCoaster(childHeight)
+{
+    if (childHeight < 52){
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+    else {
+        console.log("Get on that ride, kiddo!");
+    }
+}
+
+displayIfChildIsAbleToRideTheRollerCoaster(55);
