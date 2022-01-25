d52771640bdabe655d2644dfdef283153a2bb59b
Esokami <mollica.es@gmail.com>
2022-01-24 21:40:28 -0800

    Submitting updated Hello_Flask. Adding Playground assignment

--- Python/Flask/Fundamentals/Hello_Flask/server.py
+++ Python/Flask/Fundamentals/Hello_Flask/server.py
@@ -1,9 +1,14 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)   # Create a new instance of the Flask class called "app"
 @app.route("/")               # The "@" decorator associates this route with the function
 # immediately following
-def hello_world():
-    return "Hello World!"     # Return the string "Hello World!" as a response
+def index():
+    # Instead of returning a string, we'll return the result of the render_template method, passing
+    # in the name of our HTML file
+    return render_template("index.html")
+
+# def hello_world():
+#     return "Hello World!"     # Return the string "Hello World!" as a response
 
 @app.route("/success")
 def success():
@@ -11,7 +16,8 @@ def success():
 
 @app.route("/hello/<string:name>/<int:number>")
 def hello(name, number):
-    return f"Hello {name * number}"
+    # return f"Hello {name * number}"
+    return render_template("hello.html", name=name, number=number)
 
 @app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
 def show_user_profile(username, id):
