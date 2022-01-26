313ffd30eb0382cdd8bfca0bbcc24303974470bb
Esokami <mollica.es@gmail.com>
2022-01-25 19:07:14 -0800

    Adding updated Playground assignment

--- Python/Flask/Fundamentals/Playground/server.py
+++ Python/Flask/Fundamentals/Playground/server.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.route("/play")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", number = 3, color = "powderblue")
+
+@app.route("/play/<int:number>")
+def multiply(number):
+    return render_template("index.html", number = number, color ="powderblue")
+
+@app.route("/play/<int:number>/<string:color>")
+def color(number, color):
+    return render_template("index.html", number = number, color = color)
 
 if __name__=="__main__":
     app.run(debug = True)
