f28cb636d5d11bfd343e09375436ee13d6efc0b8
Esokami <mollica.es@gmail.com>
2022-01-31 18:05:09 -0800

    Committing finished Checkerboard assignment

--- Python/Flask/Fundamentals/Checkerboard/server.py
+++ Python/Flask/Fundamentals/Checkerboard/server.py
@@ -9,5 +9,13 @@ def index():
 def changeHeight(number):
     return render_template("index.html", number = number)
 
+@app.route("/<int:number>/<int:num2>")
+def changeBoth(number, num2):
+    return render_template("index.html", number = number, num2 = num2)
+
+@app.route("/<int:number>/<int:num2>/<string:color_1>/<string:color_2>")
+def alternateColors(number, num2, color_1, color_2):
+    return render_template("index.html", number = number, num2 = num2, color_1 = color_1, color_2 = color_2)
+
 if __name__=="__main__":
     app.run(debug=True)
