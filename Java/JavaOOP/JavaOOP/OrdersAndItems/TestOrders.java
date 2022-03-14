5d7edaf2504a339c0ca5d41aa394ec92f7e7f7e1
Esokami <mollica.es@gmail.com>
2022-03-13 17:53:59 -0700

    Committing files from Java OOP section

---
+++ Java/JavaOOP/JavaOOP/OrdersAndItems/TestOrders.java
@@ -0,0 +1,103 @@
+public class TestOrders {
+    public static void main(String[] args){
+        // Menu Items
+        Item item1 = new Item();
+        item1.name = "Latte";
+        item1.price = 3.8;
+
+        Item item2 = new Item();
+        item2.name = "Mocha";
+        item2.price = 3.5;
+
+        Item item3 = new Item();
+        item3.name  = "Cappuccino";
+        item3.price = 4.2;
+
+        Item item4 = new Item();
+        item4.name = "Drip";
+        item4.price = 3.6;
+
+        // Order variables -- order1, order2, etc
+        Order order1 = new Order();
+        order1.name = "Cindhurri";
+        order1.total = 0.0;
+        order1.ready = false;
+
+        Order order2 = new Order();
+        order2.name = "Jimmy";
+        order2.total = 0.0;
+        order2.ready = false;
+
+        Order order3 = new Order();
+        order3.name = "Noah";
+        order3.total = 0.0;
+        order3.ready = false;
+
+        Order order4 = new Order();
+        order4.name = "Sam";
+        order4.total = 0.0;
+        order4.ready = false;
+
+        //Application simulations
+        // Add item1 to order2 and increment total
+        order2.items.add(item1);
+        order2.total += item1.price;
+
+        // Add cappuccino to order3 and increment total
+        order3.items.add(item3);
+        order3.total += item3.price;
+
+        // Add latte to order4 and increment total
+        order4.items.add(item1);
+        order4.total += item1.price;
+
+        //Update Cindhuri's order to be ready
+        order1.ready = true;
+
+        // Sam ordered 2 more lattes. Update his order
+        order4.items.add(item1);
+        order4.items.add(item1);
+        order4.total += item1.price + item1.price;
+
+        // Update Jimmy's order to be ready
+        order2.ready = true;
+
+        // Use this example code to test various orders' updates
+        System.out.printf("Name: %s\n", order1.name);
+        System.out.printf("Total: %s\n", order1.total);
+        System.out.printf("Ready: %s\n", order1.ready);
+
+        // Print order1 variable
+        System.out.println(order1);
+
+        // Print order2 with incremented total
+        System.out.printf("Name: %s\n", order2.name);
+        System.out.printf("Total: %s\n", order2.total);
+        System.out.printf("Ready: %s\n", order2.ready);
+
+        // Print order3 
+        System.out.printf("Name: %s\n", order3.name);
+        System.out.printf("Total: %s\n", order3.total);
+        System.out.printf("Ready: %s\n", order3.ready);
+
+        // Print order4
+        System.out.printf("Name: %s\n", order4.name);
+        System.out.printf("Total: %s\n", order4.total);
+        System.out.printf("Ready: %s\n", order4.ready);
+
+        // Print Cindhuri's order status
+        System.out.printf("Name: %s\n", order1.name);
+        System.out.printf("Total: %s\n", order1.total);
+        System.out.printf("Readyl: %s\n", order1.ready);
+
+        // Print Sam's updated order
+        System.out.printf("Name: %s\n", order4.name);
+        System.out.printf("Total: %s\n", order4.total);
+        System.out.printf("Ready: %s\n", order4.ready);
+
+        //Print Jimmy's order status
+        System.out.printf("Name: %s\n", order2.name);
+        System.out.printf("Readyl: %s\n", order2.ready);
+    }
+
+}
