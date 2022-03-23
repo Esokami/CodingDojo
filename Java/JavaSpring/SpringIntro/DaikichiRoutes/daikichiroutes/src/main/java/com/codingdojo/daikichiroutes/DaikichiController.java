8a79a9e52b6dc210cee2a2d600ec72e3a796ae93
Esokami <mollica.es@gmail.com>
2022-03-22 21:18:34 -0700

    Committing updated Spring Boot projects

--- Java/JavaSpring/SpringIntro/DaikichiRoutes/daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/DaikichiController.java
+++ Java/JavaSpring/SpringIntro/DaikichiRoutes/daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/DaikichiController.java
@@ -1,6 +1,8 @@
 package com.codingdojo.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +21,20 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{destination}")
+	public String travel(@PathVariable("destination") String destination) {
+		return String.format("Congratulations! You will soon travel to %s!", destination);
+	}
+	
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") Integer num) {
+		if (num % 2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend with family and friends.";
+		}
 	}
 }
--- Java/JavaSpring/SpringIntro/HelloWorld/helloWorld/src/main/java/com/codingdojo/helloworld/HomeController.java
+++ Java/JavaSpring/SpringIntro/HelloWorld/helloWorld/src/main/java/com/codingdojo/helloworld/HomeController.java
@@ -1,10 +1,12 @@
 package com.codingdojo.helloworld;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/greeting")
+//@RequestMapping("/greeting")
 public class HomeController {
 	
 	@RequestMapping("")
@@ -12,6 +14,23 @@ public class HomeController {
 	{
 		return "This is accessed view http://your_server/simple/root";
 	}
+	
+	@RequestMapping("/")
+	public String index(@RequestParam(value="q", required=false) String searchQuery) {
+		if (searchQuery == null) {
+			return "You searched for: nothing";
+		}
+		else {
+			return "You searched for: " + searchQuery;
+		}
+		
+	}
+	
+	@RequestMapping("/m/{track}/{module}/{lesson}")
+	public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson) {
+		return "Track: " + track + ", Module: " + module + ", Lession: " + lesson;
+	}
+	
 	
 	@RequestMapping("/hello")
 	public String hello() {
