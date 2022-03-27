3b991d5f3bf2b52b598d6bc4ab1831ee6281fbbe
Esokami <mollica.es@gmail.com>
2022-03-26 17:10:25 -0700

    Adding FruityLoops assignment and updating HelloWorld

--- Java/JavaSpring/SpringIntro/HelloWorld/src/main/java/com/codingdojo/helloworld/controllers/HelloWorldController.java
+++ Java/JavaSpring/SpringIntro/HelloWorld/src/main/java/com/codingdojo/helloworld/controllers/HelloWorldController.java
@@ -1,5 +1,25 @@
 package com.codingdojo.helloworld.controllers;
 
-public class HelloWorldController {
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/dojos")
+	public String dojos(Model model) {
+		ArrayList<String> dojos = new ArrayList<String>();
+		dojos.add("Burbank");
+		dojos.add("Chicago");
+		dojos.add("Bellevue");
+		model.addAttribute("dojosFromMyController", dojos);
+		return "index.jsp";
+	}
 }
