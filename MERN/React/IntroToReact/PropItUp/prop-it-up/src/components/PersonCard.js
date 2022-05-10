001d609ea6d961b0ccb4d8c0dbea2889f5a8bbe2
Esokami <mollica.es@gmail.com>
2022-05-09 20:36:46 -0700

    Committing PuttingItTogether

--- MERN/React/IntroToReact/PropItUp/prop-it-up/src/components/PersonCard.js
+++ MERN/React/IntroToReact/PropItUp/prop-it-up/src/components/PersonCard.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PersonCard = (props) => {
+    const [ birthday, setBirthday ] = useState(props.age);
     return (
         <div>
             <h3>{props.lastName}, {props.firstName}</h3>
-            <p>Age: {props.age}</p>
+            <p>Age: {birthday}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={ (event) => setBirthday(birthday + 1) }>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
     )
 }
