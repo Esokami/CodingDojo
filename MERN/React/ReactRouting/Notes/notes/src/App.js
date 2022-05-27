a667d620cc0cd8abbc75d2d6498af454265f7606
Esokami <mollica.es@gmail.com>
2022-05-26 20:54:37 -0700

    Committing notes

---
+++ MERN/React/ReactRouting/Notes/notes/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter,
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Routing Example</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
